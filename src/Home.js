4d60e73e7f717e1c30b9e786c9704402591ab6f4
Mugiwara19238 <arnavgoel322@gmail.com>
2024-09-23 19:28:22 +0530

    update color

--- src/Home.js
+++ src/Home.js
@@ -105,7 +105,7 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
     },
     buyerContainer: {
-        backgroundColor: '#E2FFF6',
+        backgroundColor: '#000000',
         padding: 16,
         margin: 16,
         borderRadius: 8,
