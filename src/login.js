b1a4a8dd0afad0f3501772df93e446cf32c1f080
VeerSinghGamer <81002766+aashmeetsingh@users.noreply.github.com>
2024-09-24 20:11:50 +0530

    icons fix

--- src/login.js
+++ src/login.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, TextInput, TouchableOpacity, Button } from 'react-native';
+import { View, Text, StyleSheet, TextInput, TouchableOpacity, Button, Image } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function AuthScreen({ navigation }) {
