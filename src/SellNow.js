98f15e1e1fa1657ed59c92bde5ad25a0815bf4f0
Mugiwara19238 <arnavgoel322@gmail.com>
2024-09-27 12:34:51 +0530

    Sell now

--- src/SellNow.js
+++ src/SellNow.js
@@ -16,4 +16,40 @@ export default function SellNow({ navigation }) {
                         <Button title="Buyer" onPress={() => navigation.navigate("SellNow")} color="#10B981" />
                     </TouchableOpacity>
                 </View>
+
+                <View style={style.product}>
+                    <View>
+                        <Text style={styles.productTitle}>Type of Product</Text>
+                        <TouchableOpacity style={styles.orderButton}>
+                            <Text style={styles.orderButtonText}> type </Text>
+                        </TouchableOpacity>
+                    </View>
+                 </View>
+                 <View>
+                      <View style={styles.earningsContainer}>
+                         <Text style={styles.text}>Earnings</Text>
+                </View>
+                </View>
+                      <View style={styles.trackingContainer}>
+                         <Text style={styles.text}>Tracking</Text>
+                </View>
+                     <View style={styles.historyContainer}>
+                         <Text style={styles.text}>History</Text>
+                </View>
+                 
+                <Image
+                        source={{ uri: 'https://placehold.co/100x100' }}
+                        style={styles.image}
+                    />
+                <View style={styles.grid}>   
+                    {categories.map((category, index) => (
+                        <View key={index} style={styles.gridItem}>
+                            <Image
+                                source={category.image}
+                                style={styles.gridImage}/>
+                            <Text style={styles.gridText}>{category.title}</Text>
+                        </View>
+                    ))}
+                </View>
                 </ScrollView>
+                                 
